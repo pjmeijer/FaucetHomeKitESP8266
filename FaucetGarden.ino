0a70ad350ee0196d9b250bc0abb1862399719b9a
pjmeijer <perjohanmeijer@gmail.com>
2023-06-23 19:52:52 +0200

    Update FaucetGarden.ino

--- FaucetGarden.ino
+++ FaucetGarden.ino
@@ -1,3 +1,29 @@
+/*
+ * This code, implementing two faucet switches using ESP8266, was adapted using the following code and Arduino-HomeKit-ESP8266. 
+ * https://github.com/Mixiaoxiao/Arduino-HomeKit-ESP8266/tree/master
+ *
+ * switch.ino
+ *
+ *  Created on: 2020-05-15
+ *      Author: Mixiaoxiao (Wang Bin)
+ *
+ * HAP section 8.38 Switch
+ * An accessory contains a switch.
+ *
+ * This example shows how to:
+ * 1. define a switch accessory and its characteristics (in my_accessory.c).
+ * 2. get the switch-event sent from iOS Home APP.
+ * 3. report the switch value to HomeKit.
+ *
+ * You should:
+ * 1. read and use the Example01_TemperatureSensor with detailed comments
+ *    to know the basic concept and usage of this library before other examples。
+ * 2. erase the full flash or call homekit_storage_reset() in setup()
+ *    to remove the previous HomeKit pairing storage and
+ *    enable the pairing with the new accessory of this new HomeKit example.
+ */
+
+
 #include <Arduino.h>
 #include <arduino_homekit_server.h>
 #include "wifi_info.h"
